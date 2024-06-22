16a3c3b0eaa6fe2604ba5df27fdd67fb9dfa1ed4
susch19 <suschi@ist-einmalig.de>
2024-06-22 11:19:38 +0200

    Improved heart read console application
    * so that non beatsaber is also supported

---
+++ HeartBeatSensorReader/GoogleFit.cs
@@ -0,0 +1,351 @@
+﻿using Google.Apis.Fitness.v1.Data;
+using Google.Apis.Fitness.v1;
+using Google.Apis.Services;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using System.Threading;
+using System.Data;
+using Newtonsoft.Json;
+using System.Diagnostics.CodeAnalysis;
+using Google.Apis.Util;
+
+namespace HeartBeatSensorReader;
+internal class GoogleFit
+{
+    public static bool deleteAll = false;
+    private static UserCredential credential;
+    const string DataStreamId = "raw:com.google.heart_rate.bpm:248114505875";
+    const string DataStreamIdActivity = "derived:com.google.activity.segment:248114505875";
+
+    public static async Task UploadFromFiles(string path, FitnessService? service = null)
+    {
+        if (service is null)
+            service = await GetFitnessService();
+
+        await CreateDefaultDataSources(service);
+
+        if (deleteAll)
+        {
+
+            var sessionToDelete = await service.Users.Sessions.List("me").ExecuteAsync();
+            foreach (var item in sessionToDelete.Session)
+            {
+                if (item.Name == "Beatsaber VR Workout")
+                    try
+                    {
+                        var sessionDelete = await service.Users.Sessions.Delete("me", item.Id).ExecuteAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+            }
+        }
+
+        var pointsExisting = Directory.GetFiles(path, "BPMoverTime_*.txt", SearchOption.TopDirectoryOnly);
+        var regex = new Regex(@"(\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2}): (\d+)");
+
+
+        await UploadFiles(pointsExisting, regex, "dd.MM.yyyy HH:mm:ss");
+        pointsExisting = Directory.GetFiles(path, "BPMoverTimeNew_*.txt", SearchOption.TopDirectoryOnly);
+        regex = new Regex(@"(\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2}\.\d{4}): (\d+)");
+
+        await UploadFiles(pointsExisting, regex, "dd.MM.yyyy HH:mm:ss.ffff");
+
+    }
+
+    private static async Task UploadFiles(string[] pointsExisting, Regex regex, [StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string dateFormat)
+    {
+        List<BpmData> datas = new();
+        foreach (var file in pointsExisting)
+        {
+            if (file.Contains("not a number"))
+            {
+                continue;
+            }
+
+            foreach (var line in File.ReadAllLines(file))
+            {
+                Match match = regex.Match(line);
+                if (match.Success)
+                {
+                    string datetimeString = match.Groups[1].Value;
+                    string heartrateString = match.Groups[2].Value;
+
+
+                    DateTime datetime = DateTime.ParseExact(datetimeString, dateFormat, null);
+                    int heartrate = int.Parse(heartrateString);
+                    datas.Add(new(datetime, heartrate, GetNanoSecondsSinceEpochFrom(datetime)));
+                }
+                else
+                {
+                    Console.WriteLine("No match found");
+                }
+
+            }
+
+
+            await UploadData(datas);
+            var fi = new FileInfo(file);
+            File.Move(fi.FullName, Path.Combine(fi.Directory.FullName, "BPMDone", fi.Name));
+            Console.WriteLine($"Processed {file} with {datas.Count} datas");
+            datas.Clear();
+
+        }
+    }
+
+    public static async Task UploadData(List<BpmData> datas, bool deleteBefore = false, int activityType = 108)
+    {
+
+        using FitnessService service = await GetFitnessService();
+        if ((credential.Token.IssuedUtc.AddSeconds(credential.Token.ExpiresInSeconds.Value) - DateTime.UtcNow).TotalSeconds < 120)
+            await credential.RefreshTokenAsync(CancellationToken.None);
+        await UploadData(service, datas, deleteBefore, activityType);
+    }
+    public static async Task UploadData(FitnessService service, List<BpmData> datas, bool deleteBefore = false, int activityType = 108)
+    {
+        datas = datas.OrderBy(x => x.NanoSecondsSinceEpoch).ToList();
+        SortAndUniquifyDatas(datas);
+
+        var minData = datas.First();
+        var maxData = datas.Last();
+        var startMs = GetMilliSecondsSinceEpochFrom(minData.Occured);
+        var endMs = GetMilliSecondsSinceEpochFrom(maxData.Occured);
+        var sessionId = $"{(activityType == 9 ? "Aeorobic" : "Beatsaber")}_{startMs}-{endMs}";
+
+        var dataset = new Dataset
+        {
+            DataSourceId = DataStreamId,
+            MinStartTimeNs = GetNanoSecondsSinceEpochFrom(minData.Occured),
+            MaxEndTimeNs = GetNanoSecondsSinceEpochFrom(maxData.Occured),
+        };
+
+        var dataSetId = $"{minData.NanoSecondsSinceEpoch}-{maxData.NanoSecondsSinceEpoch}";
+        if (deleteBefore)
+        {
+
+            var deletion = await service.Users.DataSources.Datasets.Delete("me", DataStreamId, dataSetId).ExecuteAsync();
+        }
+
+
+        for (int i = 0; i <= datas.Count / 1000; i++)
+        {
+            dataset.Point = datas
+                .Skip(i * 1000)
+                .Take(1000)
+                .Select(x => new DataPoint
+                {
+                    DataTypeName = "com.google.heart_rate.bpm",
+                    StartTimeNanos = x.NanoSecondsSinceEpoch,
+                    EndTimeNanos = x.NanoSecondsSinceEpoch,
+                    Value = new List<Value> { new Value { FpVal = x.Heartrate } },
+
+                })
+                .ToList();
+
+
+            var res = await service.Users.DataSources.Datasets.Patch(dataset, "me", DataStreamId, dataSetId).ExecuteAsync();
+        }
+
+
+        var session = new Session()
+        {
+            StartTimeMillis = startMs,
+            EndTimeMillis = endMs,
+            ActiveTimeMillis = endMs - startMs,
+            ActivityType = activityType, //Andere (nicht klassifizierte Fitnessaktivitäten)
+            Application = new Application
+            {
+                Name = "Heart Beat Sensor Reader",
+                Version = "1.0"
+            },
+            Description = activityType  == 9 ? "Aeorobic" : "Beatsaber VR",
+            Name = activityType == 9 ? "Aeorobic Training" : "Beatsaber VR Workout",
+            Id = sessionId
+        };
+        session = await service.Users.Sessions.Update(session, "me", sessionId).ExecuteAsync();
+
+
+
+        var datasetActivity = new Dataset
+        {
+            DataSourceId = DataStreamIdActivity,
+            MinStartTimeNs = GetNanoSecondsSinceEpochFrom(minData.Occured),
+            MaxEndTimeNs = GetNanoSecondsSinceEpochFrom(maxData.Occured),
+        };
+
+
+        datasetActivity.Point = new List<DataPoint>() {
+                    new DataPoint
+                   {
+                       DataTypeName = "com.google.activity.segment",
+                       StartTimeNanos = minData.NanoSecondsSinceEpoch,
+                       EndTimeNanos = maxData.NanoSecondsSinceEpoch,
+                       Value = new List<Value>
+                            {
+                                new Value
+                                {
+                                    IntVal = 108, //Andere (nicht klassifizierte Fitnessaktivitäten)
+                                }
+                            }
+                   }
+                };
+
+
+        var createdActivityDataset = await service.Users.DataSources.Datasets.Patch(datasetActivity, "me", DataStreamIdActivity, dataSetId).ExecuteAsync();
+
+
+        Console.WriteLine($"Inserted {datas.Count}");
+    }
+
+    private static async Task CreateDefaultDataSources(FitnessService service)
+    {
+        DataSource dataSource = await GetDataSource(service, DataStreamId, "com.google.heart_rate.bpm",
+            new DataTypeField { Name = "bpm", Format = "floatingPoint" }, "raw");
+
+        DataSource activitySegmentDataSource = await GetDataSource(service, DataStreamIdActivity, "com.google.activity.segment", new DataTypeField { Name = "activity", Format = "integer" }, "derived");
+    }
+
+    internal static async Task<FitnessService> GetFitnessService()
+    {
+        FitnessService service;
+        try
+        {
+
+            await AuthorizeToGoogle();
+            service = new FitnessService(new BaseClientService.Initializer()
+            {
+                ApplicationName = "Heart Beat Sensor Reader",
+                HttpClientInitializer = credential,
+
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            service = await GetFitnessService();
+        }
+        return service;
+    }
+
+    private static async Task AuthorizeToGoogle()
+    {
+        if (credential is not null)
+            return;
+        using var stream = File.OpenRead("additionalfiles/client_secret.json");
+
+        credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+
+                new GoogleAuthorizationCodeFlow.Initializer
+                {
+                    ClientSecrets = GoogleClientSecrets.FromStream(stream).Secrets,
+
+
+                },
+            new string[]
+            {
+                    FitnessService.ScopeConstants.FitnessHeartRateWrite,
+                    FitnessService.ScopeConstants.FitnessHeartRateRead,
+                    FitnessService.ScopeConstants.FitnessActivityRead,
+                    FitnessService.ScopeConstants.FitnessActivityWrite,
+                    FitnessService.ScopeConstants.FitnessBodyRead,
+                    FitnessService.ScopeConstants.FitnessBodyWrite,
+            },
+            "Sascha",
+            CancellationToken.None,
+            new FileDataStore("GoogleFitnessAuth", true)
+            );
+
+        var res = await credential.RefreshTokenAsync(CancellationToken.None);
+
+    }
+    private static async Task<DataSource> GetDataSource(FitnessService service, string DataStreamId, string dataTypeName, DataTypeField field, string type)
+    {
+        DataSource dataSource = null;
+        try
+        {
+            dataSource = await service.Users.DataSources.Get("me", DataStreamId).ExecuteAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        if (dataSource is null)
+        {
+
+            dataSource = new DataSource
+            {
+                Type = type,
+                DataStreamId = DataStreamId,
+                DataType = new DataType
+                {
+                    Name = dataTypeName,
+                    Field = new List<DataTypeField>
+            {
+                            field
+            }
+                },
+                Application = new Application
+                {
+                    Name = "Heart Beat Sensor Reader",
+                    Version = "1.0"
+                }
+            };
+            var dataSourceRequest = service.Users.DataSources.Create(dataSource, "me");
+            dataSource = await dataSourceRequest.ExecuteAsync();
+
+        }
+
+        return dataSource;
+    }
+
+
+
+    private static void SortAndUniquifyDatas(List<BpmData> datas)
+    {
+        BpmData previous = datas[0];
+        for (int i = 1; i < datas.Count - 1; i++)
+        {
+            var current = datas[i];
+            if (current.Occured == previous.Occured)
+            {
+                var newDate = previous.Occured.AddSeconds(-1);
+                datas[i - 1] = new BpmData(newDate, previous.Heartrate, GetNanoSecondsSinceEpochFrom(newDate));
+                i -= 3;
+
+                if (i <= 1)
+                {
+                    previous = datas[0];
+                    i = 0;
+                }
+                else
+                {
+                    previous = datas[i - 1];
+                }
+            }
+            else
+                previous = current;
+        }
+    }
+
+    public static long GetNanoSecondsSinceEpochFrom(DateTime dt)
+    {
+        return new DateTimeOffset(dt).ToUnixTimeMilliseconds() * 1000000;
+    }
+
+    public static long GetMilliSecondsSinceEpochFrom(DateTime dt)
+    {
+        return new DateTimeOffset(dt).ToUnixTimeMilliseconds();
+    }
+
+}
--- HeartBeatSensorReader/Program.cs
+++ HeartBeatSensorReader/Program.cs
@@ -9,80 +9,197 @@ using Windows.Storage.Streams;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using System.Net.Http;
 using System.Net;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace HeartBeatSensorReader
 {
+    public record struct BpmData(DateTime Occured, int Heartrate, long NanoSecondsSinceEpoch);
+
     public class Program
     {
         private readonly HttpClient client = new HttpClient();
-        static void Main(string[] args)
+        private static BPMMeasurement lastMeasurement;
+        private static HttpListener server;
+
+        private static List<BpmData> datas = new();
+        public static bool UseGoogleFit = true;
+        private static sbyte batteryPercent = -1;
+
+        static async Task Main(string[] args)
         {
+            Console.WriteLine("Use google fit support? (Y/n)");
+
+            ConsoleKeyInfo a;
+            while ((a = Console.ReadKey()).Key is not ConsoleKey.Y and not ConsoleKey.N)
+            {
+                Console.WriteLine("Use google fit support? (Y/n)");
+            }
+            UseGoogleFit = a.Key == ConsoleKey.Y;
+
+            if (UseGoogleFit)
+            {
+
+                using var service = await GoogleFit.GetFitnessService();
+                Console.WriteLine("Connected to google fit services");
+                await GoogleFit.UploadFromFiles(@"F:\Temp", service);
+
+                Console.WriteLine("Uploaded old files");
+            }
+
+
             var p = new Program();
-            p.SubscribeBLE();
-            Console.ReadLine();
+
+            bool isBeatsaber = true;
+
+            if (UseGoogleFit)
+            {
+                Console.WriteLine("For aeorobic press a, for beatsaber any other key");
+                var key = Console.ReadKey();
+                isBeatsaber = key.Key != ConsoleKey.A;
+            }
+
+            try
+            {
+
+                p.SubscribeBLE();
+
+                server = new HttpListener();
+                server.Prefixes.Add("http://localhost:32667/");
+
+                server.Start();
+
+                _ = Task.Run(
+                async () =>
+                {
+                    while (true)
+                    {
+                        var ctx = await server.GetContextAsync();
+                        var response = ctx.Response;
+                        using (var sw = new StreamWriter(response.OutputStream, leaveOpen: true))
+                        {
+                            sw.Write(System.Text.Json.JsonSerializer.Serialize(lastMeasurement));
+
+                        }
+                        response.OutputStream.Close();
+
+                    }
+
+                });
+
+                Console.ReadLine();
+
+            }
+            finally
+            {
+                if (datas.Any(x => x.Heartrate > 90))
+                {
+                    if (UseGoogleFit)
+                        await GoogleFit.UploadData(datas, activityType: isBeatsaber ? 108 : 9);
+                    File.Move($@"F:\Temp\BPMoverTimeNew_{DateTime.UtcNow.ToShortDateString()}.txt", $@"F:\Temp\BPMDone\BPMoverTimeNew_{DateTime.UtcNow.ToShortDateString()}.txt");
+
+                }
+            }
+
+
         }
+
 
         private async void SubscribeBLE()
         {
             watcher = new BluetoothLEAdvertisementWatcher();
             string todayBPMLog = DateTime.Now.ToShortDateString();
-
+            semaphore = new SemaphoreSlim(1, 1);
             watcher.ScanningMode = BluetoothLEScanningMode.Active;
             watcher.Received += Watcher_Received;
             watcher.Start();
         }
 
         BluetoothLEDevice BluetoothLEDevice;
-        GattCharacteristic Characteristic;
-        bool lockO = false;
+        SemaphoreSlim semaphore;
         BluetoothLEAdvertisementWatcher watcher;
 
         private async void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
         {
-            string localName = args.Advertisement.LocalName;
+            string localName = args.Advertisement.LocalName.ToLower();
 
             if (localName != "heart rate sensor")
                 return;
-
-            if (!lockO && (BluetoothLEDevice == null || BluetoothLEDevice.ConnectionStatus == BluetoothConnectionStatus.Disconnected))
+            await semaphore.WaitAsync();
+            try
             {
-                lockO = true;
-                BluetoothLEDevice = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
-                GattDeviceServicesResult service = await BluetoothLEDevice.GetGattServicesAsync();
-
-                foreach (GattDeviceService s in service.Services.Where(x => x.Uuid.ToString().StartsWith("0000180d")))
+                if ((BluetoothLEDevice == null || BluetoothLEDevice.ConnectionStatus == BluetoothConnectionStatus.Disconnected))
                 {
-                    var car = await s.GetCharacteristicsAsync();
-
-                    foreach (var c in car.Characteristics.Where(x => x.Uuid.ToString().StartsWith("00002a37")))
+                    BluetoothLEDevice = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+                    var services = await BluetoothLEDevice.GetGattServicesAsync();
+                    foreach (GattDeviceService s in BluetoothLEDevice.GattServices)
                     {
-                        Characteristic = c;
-                        Characteristic.ValueChanged += C_ValueChanged;
 
-                        var value_result = await c.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                        var car = await s.GetCharacteristicsAsync();
 
-                        if (value_result != GattCommunicationStatus.Success)
+                        foreach (var c in car.Characteristics)
                         {
-                            Characteristic.ValueChanged -= C_ValueChanged;
-                            Console.WriteLine($"ERROR: Gatt Subscription hat failed: {value_result}");
-                            Console.WriteLine($"{c.Uuid}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Subscription was successfull");
-                            Console.WriteLine($"{c.Uuid}");
+                            bool cont = false;
+
+                            if (s.Uuid.ToString().StartsWith("0000180d") && c.Uuid.ToString().StartsWith("00002a37"))
+                            {
+                                c.ValueChanged += HeartBeatValueChanged;
+                                cont = true;
+
+                            }
+                            if (s.Uuid.ToString().StartsWith("0000") && c.Uuid.ToString().StartsWith("00002a19"))
+                            {
+                                c.ValueChanged += BatteryValueChanged;
+                                cont = true;
+                            }
+
+                            if (!cont)
+                                continue;
+
+                            var value_result = await c.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+
+                            if (value_result != GattCommunicationStatus.Success)
+                            {
+                                c.ValueChanged -= HeartBeatValueChanged;
+                                Console.WriteLine($"ERROR: Gatt Subscription hat failed: {value_result}");
+                                Console.WriteLine($"{c.Uuid}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Subscription was successfull");
+                                Console.WriteLine($"{c.Uuid}");
+                            }
+
                         }
                     }
                 }
-                lockO = false;
+            }
+            finally
+            {
+                semaphore.Release();
             }
 
             return;
         }
 
-        private void C_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        bool b = false;
+
+
+        private void BatteryValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
         {
             var data = new byte[args.CharacteristicValue.Length];
+            var now = DateTime.UtcNow;
+            using (var reader = DataReader.FromBuffer(args.CharacteristicValue))
+            {
+                reader.ReadBytes(data);
+            }
+            batteryPercent = (sbyte)data[0];
+        }
+
+        private void HeartBeatValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var data = new byte[args.CharacteristicValue.Length];
+            var now = DateTime.UtcNow;
             using (var reader = DataReader.FromBuffer(args.CharacteristicValue))
             {
                 reader.ReadBytes(data);
@@ -90,27 +207,71 @@ namespace HeartBeatSensorReader
 
             if (data.Length > 1)
             {
-                Task.Run(() =>
-                {
-                    try
-                    {
-                        WebRequest request = WebRequest.Create("http://localhost:8880");
-                        request.Method = "POST";
-                        byte[] byteArray = Encoding.UTF8.GetBytes("HB: " + data[1].ToString() + "bpm");
-                        request.ContentType = "application/x-www-form-urlencoded";
-                        request.ContentLength = byteArray.Length;
-                        using (Stream dataStream = request.GetRequestStream())
-                            dataStream.Write(byteArray, 0, byteArray.Length);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                });
+                var flags = data[0];
+                Span<ushort> rrValues = stackalloc ushort[10];
+                ushort heartRate = 0;
+                ushort energyExpended = 0;
 
+                if ((flags & 1) > 0)
+                    heartRate = (ushort)(data[2] << 8 | data[1]);
+                else
+                    heartRate = data[1];
+
+                int offsetRRValue = 0;
+                if ((flags & 0x001) > 0)
+                {
+                    energyExpended = (ushort)(data[4] << 8 | data[3]);
+                    offsetRRValue = 2;
+                }
+
+
+                if ((flags & 0b10000) > 0)
+                {
+                    var spanIndex = 0;
+                    for (int i = 2 + offsetRRValue; i < data.Length; i += 2)
+                    {
+                        var data1 = (ushort)(data[i + 1] << 8 | data[i]);
+                        rrValues[spanIndex++] = data1;
+                    }
+
+                }
+
+
+                lastMeasurement = new(data[1], now.ToString("s") + "Z");
+                datas.Add(new BpmData(now, data[1], GoogleFit.GetNanoSecondsSinceEpochFrom(now)));
                 File.WriteAllText(@"F:\Temp\BPM.txt", "HB: " + data[1].ToString() + "bpm");
-                File.AppendAllText($@"F:\Temp\BPMoverTime_{DateTime.Now.ToShortDateString()}.txt", $"{DateTime.Now}: {data[1]}\r\n");
-                Console.WriteLine($"HeartBeat {DateTime.Now.ToLongTimeString()}: {data[1]}");
+                File.WriteAllText(@"F:\Temp\BPM.json", System.Text.Json.JsonSerializer.Serialize(new { bpm = data[1], measured_at = now.ToString("s") + "Z" }));
+                var stringToAppend = $"{now:dd.MM.yyyy HH:mm:ss.ffff}{{0}}: {data[1]}";
+                foreach (var item in rrValues)
+                {
+                    if (item > 0)
+                    {
+                        stringToAppend += " | " + item;
+                    }
+                    else
+                        break;
+                }
+
+                File.AppendAllText($@"F:\Temp\BPMoverTimeNew_{now.ToShortDateString()}.txt", $"{stringToAppend}\r\n");
+
+                Console.WriteLine(string.Format(stringToAppend, batteryPercent == -1 ? "" : $" ({batteryPercent})"));
+
+
+            }
+        }
+
+
+        private readonly record struct BPMMeasurement
+        {
+
+            [System.Text.Json.Serialization.JsonPropertyName("bpm")]
+            public int Bpm { get; init; }
+            [System.Text.Json.Serialization.JsonPropertyName("measured_at")]
+            public string MeasuredAt { get; init; }
+            public BPMMeasurement(int bpm, string measuredAt)
+            {
+                Bpm = bpm;
+                MeasuredAt = measuredAt;
             }
         }
     }
